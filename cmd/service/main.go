6b55fdcb13155d90986e438e3bd4202ed7a3d19f
Iurikman <y.m.kutyrev@gmail.com>
2024-03-15 12:28:19 +0300

    workable code

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
 	"time"
 )
 
 func main() {
-	r := mux.NewRouter()
-	var h handler
-	r.HandleFunc("/time", h.handleTime)
-	r.HandleFunc("/stats", h.handleStats)
+	r := http.NewServeMux()
+	h := handler{
+		ipStats: ipStats{
+			ipInfo: make(map[string]int),
+		},
+	}
+	r.HandleFunc("GET /time", h.handleTime)
+	r.HandleFunc("GET /stats", h.handleStats)
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: r,
@@ -29,11 +32,20 @@ type handler struct {
 }
 
 func (h *handler) handleStats(w http.ResponseWriter, r *http.Request) {
-	h.ipStats.ipInfo = make(map[string]int)
-	h.ipStats.printIpStats(w, r)
+	var ipStatInString string
+
+	for key, val := range h.ipStats.ipInfo {
+		ipStatInString += fmt.Sprint(key + " :  " + strconv.Itoa(val) + "  ||||  ")
+	}
+	_, err := w.Write([]byte(ipStatInString))
+	if err != nil {
+		return
+	}
 }
 
 func (h *handler) handleTime(w http.ResponseWriter, r *http.Request) {
+	h.ipStats.ipInfo[r.RemoteAddr]++
+
 	_, err := w.Write([]byte(time.Now().String()))
 	if err != nil {
 		return
@@ -42,23 +54,4 @@ func (h *handler) handleTime(w http.ResponseWriter, r *http.Request) {
 
 type ipStats struct {
 	ipInfo map[string]int
-}
-
-func (i *ipStats) printIpStats(w http.ResponseWriter, r *http.Request) {
-	ip := r.RemoteAddr
-	var ipStatInString string
-	_, ok := i.ipInfo[ip]
-	if ok {
-		i.ipInfo[ip]++
-	} else {
-		i.ipInfo[ip] = 1
-	}
-
-	for key, val := range i.ipInfo {
-		ipStatInString += fmt.Sprint(key + " :  " + strconv.Itoa(val) + "  ||||  ")
-	}
-	_, err := w.Write([]byte(ipStatInString))
-	if err != nil {
-		return
-	}
 }
